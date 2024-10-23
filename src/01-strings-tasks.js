c266212f19046de2a3c5cdc3677d2fdf1a3c2236
Kirill Medvedev <repkomail@gmail.com>
2024-10-23 22:07:48 +0200

    feat: implement tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -272,8 +272,14 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const suits = ['♣', '♦', '♥', '♠'];
+  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const suitIndex = suits.indexOf(value.slice(-1));
+  const rankIndex = ranks.indexOf(value.slice(0, -1));
+
+  return suitIndex * 13 + rankIndex;
 }
 
 
